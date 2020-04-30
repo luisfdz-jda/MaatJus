fe47e04dac091b627fda2dda42d930b72a22866a
luisfdz_gmt <luisfdz@gradement.com>
2020-04-30 13:16:49 +0200

    download, uncompress and untar

--- inst/app/Main.hs
+++ inst/app/Main.hs
@@ -2,12 +2,18 @@
 
 module Main where
 
-import Turtle (shell, echo, empty)
+import Turtle (shell, echo, empty, need)
 import qualified Codec.Archive.Tar as Tar
 import Network.HTTP.Simple (httpSource, getResponseBody)
+import Network.HTTP.Client (Request)
 import Conduit
+import qualified Data.Text as T
+import Data.Maybe (fromMaybe)
+import qualified Codec.Compression.GZip as GZip
+import qualified Data.ByteString.Lazy as BS
 
-type Filename = Text
+type Filename = String
+type Filepath = String
 
 installHax :: IO ()
 installHax = do
@@ -18,23 +24,34 @@ installQemu :: IO ()
 installQemu = do
   pure ()
 
-untar :: IO ()
-untar = do
-  Tar.extract "C:\\tmp\\foo1" "C:\\tmp\\acceso_rcja.tar"
+untar :: Filepath -> IO ()
+untar path = do
+  -- Tar.extract (path ++ "\\haxm.1") (path ++ "\\haxm.1.tar.gz")
+  Tar.unpack (path ++ "\\haxm.1") . Tar.read . GZip.decompress =<< BS.readFile (path ++ "\\haxm.1.tar.gz")
   pure ()
 
-downloads :: IO ()
-  download "haxm1.tar.gz" "."
+downloads :: Filepath -> IO ()
+downloads path = do
+  download "https://github.com/luisfdz-jda/arca/releases/download/arca1/haxm.1.tar.gz" $ path ++ "\\haxm.1.tar.gz"
+  -- download "https://github.com/luisfdz-jda/arca/releases/download/arca1/qemu.1.tar.gz" $ tmpPath ++ "\\qemu.1.tar.gz"
+  -- download "https://github.com/luisfdz-jda/arca/releases/download/arca1/slax.1.tar.gz" $ tmpPath ++ "\\slax.1.tar.gz"
 
-download :: Filename -> Filepath -> IO ()
-download f p = do
-  let url = "https://github.com/luisfdz-jda/arca/releases/download/arca1/" ++ f
-  runConduitRes $ httpSource url getResponseBody .| sinkFile $ p ++ "/" ++ f
+download :: Request -> Filepath -> IO ()
+download source destination = do
+  runConduitRes $ httpSource source getResponseBody .| sinkFile destination
+
+tmpPath :: IO String
+tmpPath = do
+  tempPath <- need "TMP"
+  pure $ fromMaybe "." (fmap T.unpack tempPath)
 
 main :: IO ()
 main = do
-  downloads
-  -- untar
+  putStrLn "Descargando release..."
+  path <- tmpPath
+  downloads path
+  putStrLn "Descomprimiendo..."
+  untar path
   -- installHax
   -- installQemu
 
